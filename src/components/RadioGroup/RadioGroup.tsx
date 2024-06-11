07040a0ea837ffa48f9cee2b2e047e23fad1ab93
unicoin <unicorndev727@gmail.com>
2024-06-11 17:11:12 +0900

    refactor: mapping radio buttons

--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Radio } from './Radio';
 
+interface OptionProps {
+  label: string;
+  value: string;
+}
+
+const options: OptionProps[] = [
+  { label: 'Option A', value: 'option1' },
+  { label: 'Option B', value: 'option2' },
+  { label: 'Option C', value: 'option3' }
+];
+
 export const RadioGroup: React.FC = () => {
   return (
     <div className="flex flex-col sm:flex-row gap-2">
-      <Radio
-        label="Option A"
-        name="options"
-        value="option1"
-      />
-      <Radio
-        label="Option B"
-        name="options"
-        value="option2"
-      />
-      <Radio
-        label="Option C"
-        name="options"
-        value="option3"
-      />
+      {options.map(option => (
+        <Radio
+          key={option.value}
+          label={option.label}
+          name="options"
+          value={option.value}
+        />
+      ))}
     </div>
   );
 };
